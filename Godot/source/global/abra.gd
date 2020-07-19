00fbb326a2b5b68097588b4c343a729a9c735a92
dalton5000 <marius.stieme@gmail.com>
2020-07-19 22:41:06 +0200

    add abra

---
+++ Godot/source/global/abra.gd
@@ -0,0 +1,12 @@
+extends Node
+
+var block_map = []
+
+func is_cell_blocked(pos):
+	pass
+
+func block_cell(pos):
+	pass
+
+func unblock_cell(pos):
+	pass
--- Godot/source/overworld/Overworld.gd
+++ Godot/source/overworld/Overworld.gd
@@ -3,14 +3,3 @@ class_name Overworld
 
 onready var current_map = $Map
 
-var block_map = []
-
-
-func is_cell_blocked(pos):
-	pass
-
-func block_cell(pos):
-	pass
-
-func unblock_cell(pos):
-	pass
