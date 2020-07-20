78841cf484162fe40124d753a946873521c79122
dalton5000 <marius.stieme@gmail.com>
2020-07-20 19:10:12 +0200

    add map objects

--- Godot/source/map/map_object/MapObject.gd
+++ Godot/source/map/map_object/MapObject.gd
@@ -1,6 +1,18 @@
 extends Node2D
 
+enum TILES {DOOR, BLOCK}
+
 onready var tilemap = $TileMap
 
 func _ready():
-	$TileMap.hide()
+	tilemap.hide()
+	register_cells()
+	
+func register_cells():
+	for cell in tilemap.get_used_cells():
+		match tilemap.get_cellv(cell):
+			TILES.BLOCK:
+				Abra.occupy_position(tilemap.map_to_world(cell)+position)
+			TILES.DOOR:
+				#register door
+				pass
