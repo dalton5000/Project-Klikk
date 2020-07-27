1331cb160c81f485a477551eb44d8b40a5789cfe
dalton5000 <marius.stieme@gmail.com>
2020-07-27 21:05:09 +0200

    add player turn delay

--- Godot/source/actor/player/PlayerController.gd
+++ Godot/source/actor/player/PlayerController.gd
@@ -3,26 +3,48 @@ class_name PlayerController
 
 onready var player : Actor = owner
 
+var turn_delay := 0.1
+var input_timer := 0.0
+var last_input_dir := Vector2.ZERO
+
 func _ready():
+	
+	
 	Arceus.connect("dialog_started",self,"on_dialog_start")
 	Arceus.connect("dialog_ended",self,"on_dialog_end")
 	
 func _process(delta: float) -> void:
+	
+	var input_dir := Vector2.ZERO
+	
 	if player.state == player.STATES.IDLE:
-		if Input.is_action_pressed("ui_left"):
-			player.move(Vector2.LEFT)
+		if Input.is_action_pressed("ui_left"):			
+			input_dir = Vector2.LEFT
 		elif Input.is_action_pressed("ui_right"):
-			player.move(Vector2.RIGHT)
+			input_dir = Vector2.RIGHT
 		elif Input.is_action_pressed("ui_up"):
-			player.move(Vector2.UP)
+			input_dir = Vector2.UP
 		elif Input.is_action_pressed("ui_down"):
-			player.move(Vector2.DOWN)
-			
-		elif Input.is_action_just_pressed("interact"):
+			input_dir = Vector2.DOWN
+		
+		if input_dir != Vector2.ZERO:
+			input_timer += delta
+			if input_dir != last_input_dir:
+				player.turn(input_dir)
+				last_input_dir = input_dir
+				input_timer = 0.0
+			elif input_timer > turn_delay:
+				player.move(input_dir)
+				
+		else:
+			last_input_dir = Vector2.ZERO
+		
+		
+		if Input.is_action_just_pressed("interact"):
 			var cell = Abra.world_to_map(player.position) + player.dir
 			if Abra.is_interactable_on_cell(cell):
 				Abra.get_interactable_on_cell(cell)._interact()
-
+				
 func on_dialog_start():
 	player.change_state(player.STATES.WAIT)
 
