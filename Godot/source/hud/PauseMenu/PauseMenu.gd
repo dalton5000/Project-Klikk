4a7c55dee56380484d58c9d8fcfc30f270a32915
dalton5000 <marius.stieme@gmail.com>
2020-08-01 11:58:31 +0200

    remove self

--- Godot/source/hud/PauseMenu/PauseMenu.gd
+++ Godot/source/hud/PauseMenu/PauseMenu.gd
@@ -6,38 +6,38 @@ var buttonCount := 0
 var pos := 0
 
 func _ready():
-	self.buttonCount = find_node("ButtonContainer").get_child_count()
-	self.set_cursor(pos)
+	buttonCount = find_node("ButtonContainer").get_child_count()
+	set_cursor(pos)
 
 func _process(delta):
 	if   Input.is_action_just_pressed("menu") or Input.is_action_just_pressed("sprint"):
-		self.close()
+		close()
 	elif Input.is_action_just_pressed("ui_accept"):
 		print("PauseMenu: " + find_node("ButtonContainer").get_child(self.pos).name + " pressed")
 		find_node("ButtonContainer").get_child(self.pos).emit_signal("pressed")
 	elif Input.is_action_just_pressed("ui_up"):
 		print("up")
-		self.clear_prev_button()
-		self.pos -= 1
-		self.set_cursor(self.pos)
+		clear_prev_button()
+		pos -= 1
+		set_cursor(pos)
 	elif Input.is_action_just_pressed("ui_down"):
 		print("down")
-		self.clear_prev_button()
-		self.pos += 1
-		self.set_cursor(self.pos)
+		clear_prev_button()
+		pos += 1
+		set_cursor(pos)
 	
 func close():
 	Arceus.emit_signal("pause_menu_close")
-	self.queue_free()
+	queue_free()
 
 func clear_prev_button():
-	find_node("ButtonContainer").get_child(self.pos).get_child(0).text = ""
+	find_node("ButtonContainer").get_child(pos).get_child(0).text = ""
 
 func set_cursor(pos: int):
-	if (self.pos < 0):
-		self.pos = self.buttonCount - 1
-	if (self.pos >= self.buttonCount):
-		self.pos = 0
+	if (pos < 0):
+		pos = self.buttonCount - 1
+	if (pos >= self.buttonCount):
+		pos = 0
 		
-	find_node("ButtonContainer").get_child(self.pos).get_child(0).text = "->"
+	find_node("ButtonContainer").get_child(pos).get_child(0).text = "->"
 
